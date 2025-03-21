2cd025f5e08ef7b8f2263a17de002d90be1e95d1
HienHM <hienhm@bestarion.com>
2025-03-21 18:12:42 +0700

    update publish

--- publish.el
+++ publish.el
@@ -1,7 +1,5 @@
 ;; Set the package installation directory so that packages aren't stored in the
 ;; ~/.emacs.d/elpa path.
-;; Set the package installation directory so that packages aren't stored in the
-;; ~/.emacs.d/elpa path.
 (require 'package)
 (setq package-user-dir (expand-file-name "./.packages"))
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
@@ -14,33 +12,38 @@
 
 ;; Install dependencies
 (package-install 'htmlize)
+(package-install 'esxml)
 
 ;; Load the publishing system
 (require 'ox-publish)
-(require 'htmlize)
 
+(use-package htmlize
+  :ensure t)
 (use-package esxml
   :ensure t)
+
 
 ;; Customize the HTML output
 (setq org-html-validation-link nil            ;; Don't show validation link
       org-html-head-include-scripts nil       ;; Use our own scripts
       org-html-head-include-default-style nil ;; Use our own styles
-      org-html-head "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\" />")
+      ;; org-html-head-include-default-stype t   ;; current use emacs theme
+      org-html-doctype "html5"
+      org-html-html5-fancy t)
+
 ;; Define functions
-;; TODO for github pages
-(defvar lf/base-url "https://hienhm2212.github.io/my-org-website" "Base URL for GitHub Pages.")
+(defvar lf/base-url "" "Base URL for GitHub Pages.")
+;; (defvar lf/base-url "https://hienhm2212.github.io/my-org-website" "Base URL for GitHub Pages.")
 
 (defun lf/header ()
   "Return custom HTML header using XML"
   (sxml-to-xml
-   '(header
+   `(header
      (h1 "Welcome to Little Fox")
      (nav (@ (class "nav"))
-          (a (@ (class "nav-link") (href "/my-org-website/")) "Home") " "
-          (a (@ (class "nav-link") (href "/my-org-website/blogs")) "Blogs") " "
-          (a (@ (class "nav-link") (href "/my-org-website/contact")) "Contact")
-          ))))
+          (a (@ (class "nav-link") (href ,(concat lf/base-url "/"))) "Home") " "
+          (a (@ (class "nav-link") (href ,(concat lf/base-url "/blogs"))) "Blogs") " "
+          (a (@ (class "nav-link") (href ,(concat lf/base-url "/contact"))) "Contact")))))
 
 (defun lf/footer ()
   "Return custom HTML footer using XML"
@@ -48,78 +51,73 @@
    '(footer
      (p "© 2025 Little Fox. All rights reserved."))))
 
-(cl-defun lf/generate-page (title
-                            content
-                            info
-                            &key
-                            (publish-date)
-                            (head-extra)
-                            (pre-content)
-                            (exclude-header)
-                            (exclude-footer))
-  "Generate a complete HTML page with optional header, footer and metadata."
-  (concat
-   ;; "<!-- Generated on " (format-time-string "%Y-%m-%d @ %H:%M") "with Emacs Org Mode -->\n"
-   "<!DOCTYPE html>"
-   (sxml-to-xml
-    `(html (@ (lang "en"))
-           ;; Head section
-           (head
-            (meta (@ (charset "utf-8")))
-            (meta (@ (author "HienHM")))
-            (meta (@ (name "viewport")
-                     (content "width=device-width, initial-scale=1")))
-            (link (@ (rel "icon") (type "image/png") (href "/images/fox.png")))
-            (title ,(concat title " - Little Fox")))
-           ;; Body section
-           (body
-            ,@(unless exclude-header
-                `(,(lf/header)))
-            (div (@ (class "container"))
-                 (div (@ (class "site-post"))
-                      (h1 (@ (class "site-post-title")) ,title)
-                      ,(when publish-date
-                         `(p (@ (class "site-post-meta")) ,publish-date))
-                      ,(when pre-content pre-content)
-                      (div (@ (id "content")) ,content)))
-            ,@(unless exclude-footer
-                `(,(lf/footer))))))))
+;; Define a proper HTML template function
+(defun lf/html-template (contents info)
+  "Return complete document string after HTML conversion.
+CONTENTS is the transcoded contents string.
+INFO is a plist holding export options."
+  (let* ((title (org-export-data (plist-get info :title) info))
+         (publish-date (org-export-data (org-export-get-date info "%B %e, %Y") info)))
+    (concat
+     "<!DOCTYPE html>"
+     (sxml-to-xml
+      `(html (@ (lang "en"))
+             (head
+              (meta (@ (charset "utf-8")))
+              (meta (@ (name "viewport")
+                       (content "width=device-width, initial-scale=1")))
+              (meta (@ (author "HienHM")))
+              ;; (link (@ (rel "stylesheet")
+              ;;          (href "https://cdn.simplecss.org/simple.min.css")))
+              (link (@ (rel "stylesheet") (href ,(concat lf/base-url "/css/code.css"))))
+              (link (@ (rel "stylesheet") (href ,(concat lf/base-url "/css/site.css"))))
+              (link (@ (rel "icon") (type "image/png")
+                       (href ,(concat lf/base-url "/images/fox.png"))))
+              (title ,(concat title " - Little Fox")))
+             (body
+              ,(lf/header)
+              (main (@ (class "container"))
+                    (article (@ (class "site-post"))
+                             (h1 (@ (class "site-post-title")) ,title)
+                             ,(when publish-date
+                                `(p (@ (class "site-post-meta")) ,publish-date))
+                             (div (@ (id "content")) ,contents)))
+              ,(lf/footer)))))))
 
-(defun lf/org-html-template (contents backend info)
-  "Use custom HTML generation with contents, backend, and info."
-  (ignore backend) ;; Backend is not used in this function
-  (lf/generate-page (org-export-data (plist-get info :title) info)
-                    contents
-                    info
-                    :publish-date (org-export-data (org-export-get-date info "%B %e, %Y") info)))
+;; Override the default HTML template
+(setq org-html-template-function 'lf/html-template)
 
-(setq org-html-preamble nil
-      org-html-postamble nil
-      org-export-filter-final-output-functions '(lf/org-html-template))
-
-;; Define the publishing project
-(defun lf/blogs-sitemap (title files)
-  (format "#+title: %s\n\n%s"
-          title
-          (mapconcat (lambda (file)
-                       (format "- %s\n" file))
-                     (cadr files)
-                     "\n")))
+;; Very simple sitemap function that works reliably
+(defun lf/sitemap-format-entry (entry style project)
+  "Format ENTRY for the sitemap.
+ENTRY is a file name.  STYLE is the style of the sitemap.
+PROJECT is the current project."
+  (format "* [[file:%s][%s]]\nPublished: %s\n"
+          (file-name-nondirectory entry)
+          (org-publish-find-title entry project)
+          (format-time-string "%Y-%m-%d"
+                              (org-publish-find-date entry project))))
 
 (setq org-publish-project-alist
       (list '("littlefox:main"
               :base-directory "./content"
               :base-extension "org"
+              :exclude "blogs.org\\|blogs/.*\\.org"
               :publishing-directory "./public"
               :publishing-function org-html-publish-to-html
-              :with-title nil
-              :with-timestamps nil)
+              :with-toc nil           ;; Disable TOC
+              :section-numbers nil    ;; Disable section numbers
+              :with-author nil
+              :with-creator nil
+              :time-stamp-file nil)
+
             '("littlefox:assets"
               :base-directory "./assets"
               :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|woff2\\|ttf"
               :publishing-directory "./public"
               :recursive t
               :publishing-function org-publish-attachment)
+
             '("littlefox:blogs"
               :base-directory "./content/blogs"
               :base-extension "org"
@@ -127,9 +125,18 @@
               :publishing-function org-html-publish-to-html
               :auto-sitemap t
               :sitemap-title "Little Fox Blogs"
-              :sitemap-filename "../blogs.org"
-              :sitemap-function lf/blogs-sitemap)
-            ))
+              :sitemap-filename "index.org"
+              :sitemap-sort-files anti-chronologically
+              :sitemap-format-entry lf/sitemap-format-entry
+              :with-toc nil           ;; Disable TOC
+              :section-numbers nil    ;; Disable section numbers
+              :with-author nil
+              :with-creator nil
+              :time-stamp-file nil)
+
+            '("littlefox"
+              :components ("littlefox:main" "littlefox:assets" "littlefox:blogs"))))
+
 ;; Generate the site output
 (org-publish-all t)
 
